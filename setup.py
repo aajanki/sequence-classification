48d7b88e8afc865cedf799ecc0c93ae159232cd1
Antti Ajanki <antti.ajanki@iki.fi>
2018-02-15 20:29:08 +0200

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import sys
+import textwrap
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
@@ -8,8 +9,32 @@ with open('requirements.txt') as f:
 TESTS_REQUIRES = ['nose>=1.1.2']
 
 setup(name='sklearn-sequence-classifiers',
-      version='0.1',
+      version='0.2',
       description='Sequence classifiers for scikit-learn',
+      long_description=textwrap.dedent("""\
+          Sequence classifiers for scikit-learn
+          =====================================
+
+          Convolutional neural network sequence classifier with a scikit-learn interface.
+
+          Usage example
+          -------------
+
+          Predicting IMDB review sentiments.::
+
+              from keras.datasets import imdb
+              from keras.preprocessing import sequence
+              from sequence_classifiers import CNNSequenceClassifier
+
+              maxlen = 400
+              (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=5000)
+              x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+              x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+              clf = CNNSequenceClassifier(epochs=2)
+              clf.fit(x_train, y_train)
+              print(clf.score(x_test, y_test))
+          """),
       author='Antti Ajanki',
       packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
